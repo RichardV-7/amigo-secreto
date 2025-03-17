1a78e9ac2f612084966573723463b05bebf7dc44
Richard Velazquez <richard.blue3ds@gmail.com>
2025-03-17 16:34:23 -0300

    Agg primera parte

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//creo la lista en donde se guardan los nombres 
+let amigos = [];
+
+//creo la funcion del boton que se utiliza para añadir amigos
+function agregarAmigo() {
+    let ingresoNombre = document.getElementById('amigo').value;
+    
+    if (ingresoNombre === '') {
+        alert('Por favor, inserte un nombre.');
+        
+    } else {
+        amigos.push(ingresoNombre);
+    }
+
+    document.getElementById('amigo').value = '';
+}
+
